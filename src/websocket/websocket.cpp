e3b5a5123fa76446948954693d91fda4bd41bdd1
Andrii Kitsun <andrii.kitsun@gmail.com>
2024-12-28 12:34:37 +0200

    SC-63: Slow ESP IP resolution during HTTP requests (#25)

--- src/websocket/websocket.cpp
+++ src/websocket/websocket.cpp
@@ -6,11 +6,9 @@ void startWebSocket() {
   webSocket.begin();
 
   Serial.println(F("WebSocket started on:"));
-  Serial.print(F("1. IP address: ws://"));
+  Serial.print(F("ws://"));
   Serial.print(WiFi.localIP());
   Serial.println(F(":81"));
-  Serial.print(F("2. URL: "));
-  Serial.println(F("ws://" ESP_DOMAIN_NAME ".local:81"));
 }
 
 void tickWebSocket() {
