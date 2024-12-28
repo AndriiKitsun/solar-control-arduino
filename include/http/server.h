b996494bad40af2fc5b9b2f6b1fd9836f3959f53
Andrii Kitsun <andrii.kitsun@gmail.com>
2024-12-28 15:26:07 +0200

    Revert "SC-63: Slow ESP IP resolution during HTTP requests (#25)" (#26)
    This reverts commit e3b5a5123fa76446948954693d91fda4bd41bdd1.

--- include/http/server.h
+++ include/http/server.h
@@ -5,6 +5,7 @@
 #include <Arduino.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 #include "pzems/pzem.h"
 #include "utils/wifi.h"
 
--- src/websocket/websocket.cpp
+++ src/websocket/websocket.cpp
@@ -6,9 +6,11 @@ void startWebSocket() {
   webSocket.begin();
 
   Serial.println(F("WebSocket started on:"));
-  Serial.print(F("ws://"));
+  Serial.print(F("1. IP address: ws://"));
   Serial.print(WiFi.localIP());
   Serial.println(F(":81"));
+  Serial.print(F("2. URL: "));
+  Serial.println(F("ws://" ESP_DOMAIN_NAME ".local:81"));
 }
 
 void tickWebSocket() {
