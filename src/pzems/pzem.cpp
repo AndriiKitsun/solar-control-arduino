baf3527cbcab608b58aa65f993d8caf04da3bd60
Andrii Kitsun <Andrii.Kitsun@gmail.com>
2025-01-08 21:42:12 +0200

    restore AC pzems logic

--- src/pzems/pzem.cpp
+++ src/pzems/pzem.cpp
@@ -1,9 +1,9 @@
 #include "pzems/pzem.h"
 
-// static SoftwareSerial acPzemSerial(AC_PZEM_RX_PIN, AC_PZEM_TX_PIN);
+static SoftwareSerial acPzemSerial(AC_PZEM_RX_PIN, AC_PZEM_TX_PIN);
 
-// static AcPzem acInputPzem(AC_INPUT_PZEM_NAME, acPzemSerial, 0, AC_INPUT_PZEM_ADDRESS);
-// static AcPzem acOutputPzem(AC_OUTPUT_PZEM_NAME, acPzemSerial, 16, AC_OUTPUT_PZEM_ADDRESS);
+static AcPzem acInputPzem(AC_INPUT_PZEM_NAME, acPzemSerial, 0, AC_INPUT_PZEM_ADDRESS);
+static AcPzem acOutputPzem(AC_OUTPUT_PZEM_NAME, acPzemSerial, 16, AC_OUTPUT_PZEM_ADDRESS);
 
 static DcPzem dcBatteryPzem(DC_BATTERY_PZEM_NAME, DC_BATTERY_PZEM_RO_PIN, DC_BATTERY_PZEM_RE_DE_PIN, DC_BATTERY_PZEM_DI_PIN, 32, DC_BATTERY_PZEM_ADDRESS);
 
@@ -14,8 +14,8 @@ void startPzems() {
   dcBatteryPzem._preTransmissionCb = []() { dcBatteryPzem.preTransmission(); };
   dcBatteryPzem._postTransmissionCb = []() { dcBatteryPzem.postTransmission(); };
 
-  // acInputPzem.startPzem();
-  // acOutputPzem.startPzem();
+  acInputPzem.startPzem();
+  acOutputPzem.startPzem();
   dcBatteryPzem.startPzem();
 }
 
@@ -28,17 +28,17 @@ JsonDocument getPzemsPayload() {
 
   JsonArray pzems = doc[F("pzems")].to<JsonArray>();
 
-  // JsonDocument acInputValues = acInputPzem.getValues(date);
-  // JsonDocument acOutputValues = acOutputPzem.getValues(date);
+  JsonDocument acInputValues = acInputPzem.getValues(date);
+  JsonDocument acOutputValues = acOutputPzem.getValues(date);
   JsonDocument dcBatteryValues = dcBatteryPzem.getValues(date);
 
-  // if (!acInputValues.isNull()) {
-  //   pzems.add(acInputValues);
-  // }
+  if (!acInputValues.isNull()) {
+    pzems.add(acInputValues);
+  }
 
-  // if (!acOutputValues.isNull()) {
-  //   pzems.add(acOutputValues);
-  // }
+  if (!acOutputValues.isNull()) {
+    pzems.add(acOutputValues);
+  }
 
   if (!dcBatteryValues.isNull()) {
     pzems.add(dcBatteryValues);
@@ -50,8 +50,8 @@ JsonDocument getPzemsPayload() {
 JsonDocument getPzemsStatus() {
   JsonDocument doc;
 
-  // doc.add(acInputPzem.getStatus());
-  // doc.add(acOutputPzem.getStatus());
+  doc.add(acInputPzem.getStatus());
+  doc.add(acOutputPzem.getStatus());
   doc.add(dcBatteryPzem.getStatus());
 
   return doc;
@@ -60,8 +60,8 @@ JsonDocument getPzemsStatus() {
 JsonDocument resetPzemsCounter() {
   JsonDocument doc;
 
-  // doc.add(acInputPzem.resetCounter());
-  // doc.add(acOutputPzem.resetCounter());
+  doc.add(acInputPzem.resetCounter());
+  doc.add(acOutputPzem.resetCounter());
   doc.add(dcBatteryPzem.resetCounter());
 
   return doc;
@@ -71,9 +71,9 @@ JsonDocument changePzemAddress(String name, uint8_t address) {
   JsonDocument doc;
 
   if (name == F(AC_INPUT_PZEM_NAME)) {
-    // doc = acInputPzem.changeAddress(address);
+    doc = acInputPzem.changeAddress(address);
   } else if (name == F(AC_OUTPUT_PZEM_NAME)) {
-    // doc = acOutputPzem.changeAddress(address);
+    doc = acOutputPzem.changeAddress(address);
   } else if (name == F(DC_BATTERY_PZEM_NAME)) {
     doc = dcBatteryPzem.changeAddress(address);
   }
